f1177657aafe6b231943c5d513eeac76ccd7e018
VenyaNuthakki <71914805+VenyaNuthakki@users.noreply.github.com>
2020-10-03 09:17:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+var studentarray=[];
+function submit(){
+    var name1=document.getElementById("name1").value;
+    var name2=document.getElementById("name2").value;
+    var name3=document.getElementById("name3").value;
+    var name4=document.getElementById("name4").value;
+    studentarray.push(name1);
+    studentarray.push(name2);
+    studentarray.push(name3);
+    studentarray.push(name4);
+    console.log(studentarray);
+    document.getElementById("display").innerHTML=studentarray;
+    document.getElementById("submit_button").style.display="none";
+    document.getElementById("sort_button").style.display="inline-block";
+}
+function sorting(){
+    studentarray.sort();
+    console.log(studentarray);
+    document.getElementById("display").innerHTML=studentarray;
+}
